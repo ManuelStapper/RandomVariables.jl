0de3b29a29776230ff6917bfb6ffba94a3ea0088
Manuel Stapper <manuel.stapper@googlemail.com>
2022-06-28 18:28:26 +0200

    ...

--- code.jl
+++ code.jl
@@ -1,14 +1,3 @@
-using PkgTemplates
+using Main.RandomVariables
 
-tpl = Template(;
-    user = "ManuelStapper",
-    authors = "Manuel Stapper",
-    dir="~/code",
-    plugins=[
-        Git(; manifest=true, ssh=true),
-        Codecov(),
-    ],
-)
-
-cd("C:\\Users\\stapperm\\.julia\\dev\\RandomVariables")
-tpl("RandomVariables.jl")
+using Pkg
