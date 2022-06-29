9d6198fd214b311e1727ddba516fbcf167c4ef01
Manuel Stapper <manuel.stapper@googlemail.com>
2022-06-29 09:36:27 +0200

    Again

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,9 @@ using RandomVariables, Distribtutions
 using Test
 
 @testset "RandomVariables.jl" begin
-    X1 = RV(Poisson(4.2))
-    X2 = RV(Normal())
-    X3 = RV(Normal(1, 2))
+    X1 = RV(RandomVariables.Poisson(4.2))
+    X2 = RV(RandomVariables.Normal())
+    X3 = RV(RandomVariables.Normal(1, 2))
 
     A = (X1 in 1:5) & (X2 < 3) & (X2 > 0)
     B = (X1 != 2) & (log(X1 + 1) < 10)
@@ -15,7 +15,7 @@ using Test
     P(A ∨ B)
     P(A ⊻ B)
     P(!(A))
-    P(-Poisson(1.2) > -4.1)
+    P(-RandomVariables.Poisson(1.2) > -4.1)
     P(X2*4 + 2 < 2)
     P(((X3 - 1)/2)^2 <= 5.99)
     P(abs((X3 - 1)/2) <= 1.96)
