d5cabe258d560631d7c0abef6caa00babb7002c3
Manuel Stapper <manuel.stapper@googlemail.com>
2022-06-29 09:43:35 +0200

    Other tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ using Test
     X3 = RV(RandomVariables.Normal(1, 2))
 
     A = (X1 in 1:5) & (X2 < 3) & (X2 > 0)
-    B = (X1 != 2) & (log(X1 + 1) < 10)
+    B = (X1 != 2) & (log(abs(X3) + 1) < 2)
     P(A)
     P(B)
     P(A|B)
@@ -15,7 +15,7 @@ using Test
     P(A ∨ B)
     P(A ⊻ B)
     P(!(A))
-    P(-RandomVariables.Poisson(1.2) > -4.1)
+    P(-RandomVariables.Poisson(1.2) > -4)
     P(X2*4 + 2 < 2)
     P(((X3 - 1)/2)^2 <= 5.99)
     P(abs((X3 - 1)/2) <= 1.96)
